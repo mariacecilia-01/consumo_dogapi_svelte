2c266407c8fdb0626fb020387088f1d396d658b0
mariacecilia-01 <pereirajardimmariacecilia@gmail.com>
2026-01-28 11:27:22 -0300

    começamos a fazer o projeto, com estrutura basica feita e função do fetch criada.

--- SvelteDogAPI/src/routes/+page.svelte
+++ SvelteDogAPI/src/routes/+page.svelte
@@ -1,12 +1,71 @@
 <script>
-	let nome = "Mundo";
+	import { onMount } from "svelte";
+
+	async function buscandoCachorro() {
+		const input = document.getElementById("input").value;
+
+		const url = `https://dog.ceo/api/breeds/{$input}/image`;
+
+		const response = await fetch(url);
+		const dados = await response.json();
+	}
 </script>
 
-<h1>Olá, {nome}!</h1>
-<p>Este é meu primeiro componente Svelte.</p>
+<link rel="preconnect" href="https://fonts.googleapis.com" />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+<link href="https://fonts.googleapis.com/css2?family=Poetsen+One&display=swap" rel="stylesheet"
+/>
+
+<main>
+	<h1>Busca Dog</h1>
+
+	<div class="busca">
+		<input type="text" id="input" placeholder="Busque a raça do cachorro"/>
+
+			<img src="/src/img/Search.svg" alt="icone de busca" id="botaoBusca">
+
+	</div>
+
+	
+
+
+</main>
 
 <style>
+	main {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		background-color: bisque;
+		height: 100vh;
+	}
+
 	h1 {
-		color: rgb(97, 168, 145);
+		color: rgb(172, 0, 66);
+		font-family: 'Poetsen One';
+	}
+
+	.busca{
+		position: relative;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+
+	#botaoBusca{
+		position: absolute;
+		right: 0;
+		padding: 10px;
+		height: 25px;
+	
+	}
+
+	input {
+		width: 60vh;
+		height: 40px;
+		border-radius: 20px;
+		border: none;
+		text-align: center;
 	}
 </style>
